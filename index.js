ff95e3553805c9d38c92a89fe43ddb0843862ac5
Shraddhakumari2511 <shraddhamishra8977@gmail.com>
2024-03-03 13:41:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// gsap.from(".navbar p",{
+//     scale:0,
+//     opacity:0,
+//     delay: 2,
+//     duration:2,
+// })
+// gsap.from(".heading h1, .heading img",{
+//     y:-100,
+//     duration: 1,
+//     delay: 1,
+//     stagger: 0.5,
+// })
+// gsap.from(".best-teacher img",{
+//     scale:0,
+//     opacity:0,
+//     delay: 1.5,
+//     duration:2,
+// })
+// gsap.from(".box p",{
+//     scale:0,
+//     opacity:0,
+//     delay: 1.5,
+//     duration:2,
+// })
+// gsap.from(".footer p",{
+//     y:400,
+//     opacity:0,
+//     delay: 2,
+//     duration:1,
+// })
